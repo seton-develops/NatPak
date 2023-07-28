43f183cfcad19987066538211a4bffc1d2083232
Sean Tonthat <sttt2017@gmail.com>
2023-07-28 02:00:17 -0700

    -fixed unable to obtain user's coordinates. solution was to use currentlocation() instead of lastlocation()
    -added method to determine whether user has given permissions

--- app/src/main/java/com/seton_develops/natpak/MainActivity.kt
+++ app/src/main/java/com/seton_develops/natpak/MainActivity.kt
@@ -2,18 +2,28 @@ package com.seton_develops.natpak
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
+import com.google.android.gms.tasks.CancellationToken
+import com.google.android.gms.tasks.CancellationTokenSource
+import com.google.android.gms.tasks.OnTokenCanceledListener
+import com.google.android.gms.tasks.Task
 import com.seton_develops.natpak.databinding.ActivityMainBinding
+
+
+private const val REQUEST_CODE = 101
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var  binding: ActivityMainBinding
     private lateinit var fusedLocationClient: FusedLocationProviderClient
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,12 +34,27 @@ class MainActivity : AppCompatActivity() {
         fusedLocationClient= LocationServices.getFusedLocationProviderClient(this)
 
         binding.buttonTest.setOnClickListener {
-            getUserPermissions()
+            requestLocation()
             binding.textviewTest.text = "james's text"
         }
     }
 
-    private fun getUserPermissions() {
+    override fun onStart() {
+        super.onStart()
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+            requestLocation()
+        }
+        else {
+            getUserLocationPermissions()
+
+        }
+
+    }
+
+    private fun getUserLocationPermissions() {
         if (ActivityCompat.checkSelfPermission(
                 this,
                 Manifest.permission.ACCESS_FINE_LOCATION
@@ -42,16 +67,70 @@ class MainActivity : AppCompatActivity() {
             ActivityCompat.requestPermissions(
                 this,
                 arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
-                101
+                REQUEST_CODE
             )
 
         }
+
     }
 
+    @SuppressWarnings("MissingPermission") //Gets around permission check
     private fun requestLocation() {
+        var locationTask: Task<Location> = fusedLocationClient.getCurrentLocation(
+            Priority.PRIORITY_HIGH_ACCURACY,
+            object : CancellationToken() {
+                override fun onCanceledRequested(p0: OnTokenCanceledListener) =
+                    CancellationTokenSource().token
 
+                override fun isCancellationRequested() = false
+            })
 
+        locationTask.addOnSuccessListener {
+            if (it != null) {
+                Toast.makeText(this,
+                    "latitude: ${it.latitude} longitude: ${it.longitude}",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+            else { //Todo: remove this. for checking purposes only
+                Toast.makeText(this,
+                    "error",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+
+        locationTask.addOnFailureListener {
+            Toast.makeText(this, "Unable to get location", Toast.LENGTH_LONG).show()
+        }
     }
+
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+
+        //Permission granted
+        if (requestCode == REQUEST_CODE &&
+            grantResults.isNotEmpty() &&
+            grantResults[0] == PackageManager.PERMISSION_GRANTED ) {
+
+            requestLocation()
+        }
+        //Permission has not been granted
+        else {
+            Toast.makeText(this,
+                "Location Tracking not granted. Enable permissions to enable tracking",
+                Toast.LENGTH_LONG).show()
+        }
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+
 
 
 }
